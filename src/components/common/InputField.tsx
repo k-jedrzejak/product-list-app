5f019d3c2cc6cc4bf9f8ea1d5fca5d3b48090814
k-jedrzejak <kjedrzejak91@gmail.com>
2024-07-14 14:55:30 +0200

    InputField: update conditional rendering

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
+import React from "react";
 
 interface InputFieldProps {
-  type: 'text' | 'textarea';
+  type: "text" | "textarea";
   value: string;
   name: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
 const InputField = ({ type, value, name, onChange }: InputFieldProps) => {
-  if (type === 'textarea') {
-    return (
-      <textarea
-        className="form-control"
-        value={value}
-        onChange={onChange}
-        name={name}
-      />
-    );
-  } else {
-    return (
-      <input
-        type="text"
-        className="form-control"
-        value={value}
-        onChange={onChange}
-        name={name}
-      />
-    );
-  }
+  return (
+    <>
+      {type === "textarea" ? (
+        <textarea
+          className="form-control"
+          value={value}
+          onChange={onChange}
+          name={name}
+        />
+      ) : (
+        <input
+          type="text"
+          className="form-control"
+          value={value}
+          onChange={onChange}
+          name={name}
+        />
+      )}
+    </>
+  );
 };
 
 export default InputField;
