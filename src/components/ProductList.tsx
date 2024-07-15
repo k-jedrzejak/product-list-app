f819e4e0129ea449415b4096734f4d1096685fc4
k-jedrzejak <kjedrzejak91@gmail.com>
2024-07-15 16:54:29 +0200

    add React.memo to ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,15 +1,16 @@
-// ProductList.tsx
-
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../state/hooks';
-import { fetchProducts } from '../state/productSlice';
-import { RootState } from '../state/store';
-import { API_BASE_URL } from '../constants/constants';
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../state/hooks";
+import { fetchProducts } from "../state/productSlice";
+import { RootState } from "../state/store";
+import { API_BASE_URL } from "../constants/constants";
+import React from "react";
 
 const ProductList = () => {
   const dispatch = useAppDispatch();
-  const { products, loading, error } = useAppSelector((state: RootState) => state.products);
+  const { products, loading, error } = useAppSelector(
+    (state: RootState) => state.products
+  );
 
   useEffect(() => {
     const controller = new AbortController();
@@ -23,25 +24,31 @@ const ProductList = () => {
 
   return (
     <>
-    {loading && <div className="text-center my-5">Loading...</div>}
-    {error && <div className="alert alert-danger" role="alert">Error: {error}</div>}
-    {!loading && !error && (
-      <div className="container mt-5">
-        <h2 className="mb-4">Product List</h2>
-        <ul className="list-group">
-          {products.map((product) => (
-            <li key={product.id} className="list-group-item">
-              <Link to={`/products/${product.id}`} className="text-decoration-none">
-                {product.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )}
-</>
-
+      {loading && <div className="text-center my-5">Loading...</div>}
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          Error: {error}
+        </div>
+      )}
+      {!loading && !error && (
+        <div className="container mt-5">
+          <h2 className="mb-4">Product List</h2>
+          <ul className="list-group">
+            {products.map((product) => (
+              <li key={product.id} className="list-group-item">
+                <Link
+                  to={`/products/${product.id}`}
+                  className="text-decoration-none"
+                >
+                  {product.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
-export default ProductList;
+export default React.memo(ProductList);
