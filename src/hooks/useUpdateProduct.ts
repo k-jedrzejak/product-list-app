cdc15d0275a64dc6906f9759c1e33e7005e2bdb5
k-jedrzejak <kjedrzejak91@gmail.com>
2024-07-24 18:26:24 +0200

    remove saving state

--- src/hooks/useUpdateProduct.ts
+++ src/hooks/useUpdateProduct.ts
@@ -1,31 +1,21 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { useAppDispatch } from '../state/hooks';
 import { updateProduct } from '../state/productSlice';
 import { Product } from '../types/product';
 import { API_BASE_URL } from '../constants/constants';
 
-interface SavingState {
-  [key: string]: boolean;
-}
-
 const useUpdateProduct = (selectedProduct: Product | null, editableProduct: Partial<Product>) => {
   const dispatch = useAppDispatch();
-  const [savingState, setSavingState] = useState<SavingState>({});
-
-  const handleSave = useCallback(async (field: string, index?: number) => {
+  const handleSave = useCallback(async (field: string) => {
     let updatedProduct: Product;
 
     if (field.startsWith('image')) {
-      setSavingState((prev) => ({ ...prev, [`image-${index}`]: true }));
-
       const updatedImages = [...(editableProduct.images || [])];
       updatedProduct = {
         ...selectedProduct!,
         images: updatedImages,
       };
     } else {
-      setSavingState((prev) => ({ ...prev, [field]: true }));
-
       updatedProduct = {
         ...selectedProduct!,
         [field]: editableProduct[field as keyof Product] ?? selectedProduct![field as keyof Product],
@@ -36,17 +26,10 @@ const useUpdateProduct = (selectedProduct: Product | null, editableProduct: Part
       await dispatch(updateProduct(API_BASE_URL, updatedProduct));
     } catch (error) {
       console.error(`Error updating ${field}:`, error);
-    } finally {
-      if (field.startsWith('image')) {
-        setSavingState((prev) => ({ ...prev, [`image-${index}`]: false }));
-      } else {
-        setSavingState((prev) => ({ ...prev, [field]: false }));
-      }
-    }
+    } 
   }, [dispatch, selectedProduct, editableProduct]);
 
   return {
-    savingState,
     handleSave,
   };
 };
